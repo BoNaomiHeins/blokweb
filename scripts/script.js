ad6731eb51a93877bf7bc28003ae3577fcbb964f
BoNaomiHeins <65015999+BoNaomiHeins@users.noreply.github.com>
2020-09-17 11:56:37 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+var winkelwagen = document.querySelector("header section:nth-of-type(2) ul li:nth-of-type(2)");
+var element = document.querySelector("header p");
+
+winkelwagen.addEventListener("mouseover", function(){
+  element.classList.add("anders");
+})
+
+winkelwagen.addEventListener("mouseout", function(){
+  element.classList.remove("anders");
+})
+
+var zoeken = document.querySelector("header section:nth-of-type(2) ul li:first-of-type");
+var zoekbalk = document.querySelector("form input");
+
+zoeken.addEventListener("click", function(){
+  zoekbalk.classList.toggle("zichtbaar");
+})
