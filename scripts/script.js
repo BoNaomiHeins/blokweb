07569f731f44843e2082873a36355fe9edf9c7f9
BoNaomiHeins <65015999+BoNaomiHeins@users.noreply.github.com>
2020-09-24 15:51:29 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 // JavaScript Document
-var winkelwagen = document.querySelector("header section:nth-of-type(2) ul li:nth-of-type(2)");
+var winkelwagen = document.querySelector("header div:nth-of-type(2) ul li:nth-of-type(2)");
 var element = document.querySelector("header p");
+var zoeken = document.querySelector("header div:nth-of-type(2) ul li:first-of-type");
+var zoekbalk = document.querySelector("form input");
+var imgElement = document.querySelector(".afbeeldingveranderen");
 
 winkelwagen.addEventListener("mouseover", function(){
   element.classList.add("anders");
@@ -10,9 +13,17 @@ winkelwagen.addEventListener("mouseout", function(){
   element.classList.remove("anders");
 })
 
-var zoeken = document.querySelector("header section:nth-of-type(2) ul li:first-of-type");
-var zoekbalk = document.querySelector("form input");
 
 zoeken.addEventListener("click", function(){
   zoekbalk.classList.toggle("zichtbaar");
 })
+
+
+imgElement.addEventListener("mouseover", function(){
+    imgElement.src = "images/13.0218.03blakeskuster5.jpg";
+})
+
+imgElement.addEventListener("mouseout", function(){
+    imgElement.src = "images/13.0216.03blakeskuster_1.jpg";
+})
+
